d3aed094b3dabe32ea3ff220f9feb9826427b074
stla <laurent_step@yahoo.fr>
2014-07-13 21:20:07 +0200

    module

--- assets/Julia/module/Jgfi.jl
+++ assets/Julia/module/Jgfi.jl
@@ -123,19 +123,23 @@ end
 
 inference = function(vertex, weight, alpha)
     vertex=vec(vertex)
-  out = zeros(Float64,4)
-  #names(out) = c("mean","median","low","up")
-  out[1] = dot(vertex,weight)
-  h = [vertex weight]
-  hsort = h[sortperm(vertex),:]
-  hsum = cumsum(hsort[:,2])
-  ci_u = minimum(find(hsum.>=1-alpha/2)) #upper confidence bound
-  ci_l = minimum(find(hsum.>=alpha/2))   #lower confidence bound
-  ci_m = minimum(find(hsum.>=0.5))
-  out[3] = hsort[ci_l,1]  #lower bound
-  out[4] = hsort[ci_u,1] #upper bound
-  out[2] = hsort[ci_m,1] #estimate
-  return out
+    out = zeros(Float64,4)
+    #names(out) = c("mean","median","low","up")
+    out[1] = dot(vertex,weight)
+    h = [vertex weight]
+    hsort = h[sortperm(vertex),:]
+    hsum = cumsum(hsort[:,2])
+    ci_u = minimum(find(hsum.>=1-alpha/2)) #upper confidence bound
+    ci_l = minimum(find(hsum.>=alpha/2))   #lower confidence bound
+    ci_m = minimum(find(hsum.>=0.5))
+    out[3] = hsort[ci_l,1]  #lower bound
+    out[4] = hsort[ci_u,1] #upper bound
+    out[2] = hsort[ci_m,1] #estimate
+    println("mean: ", out[1])
+    println("median: ", out[2])
+    println("lower bound: ", out[3])
+    println("upper bound: ", out[4])
+    return out
 end
 
 
